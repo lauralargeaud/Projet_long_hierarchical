78fb2958c85968fc71563c910f28a622aea6518f
Nathan Barraud <nathbarraud31@gmail.com>
2025-02-06 12:07:33 +0100

    Removed useless transposition

--- scripts/logic_seg_utils.py
+++ scripts/logic_seg_utils.py
@@ -24,6 +24,6 @@ def get_tree_matrices(path_to_csv_tree, verbose=False):
   peer_matrix = np.matmul(H.T,H)
   peer_matrix = np.maximum(peer_matrix - np.eye(n),0)
 
-  M = np.sum(peer_matrix, axis=0).T # peer_matrix est symétrique donc l'axe n'est pas important
+  M = np.sum(peer_matrix, axis=0) # peer_matrix est symétrique donc l'axe n'est pas important
   
   return H, peer_matrix, M
