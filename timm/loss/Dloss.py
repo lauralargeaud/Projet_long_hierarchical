e1b788bfad5bd362e1f28a7372b28225ce44f18b
Nathan Barraud <nathbarraud31@gmail.com>
2025-02-06 12:06:15 +0100

    Fixed M not beeing transposed, fixed get_tree_matrices call in Dloss

--- timm/loss/Dloss.py
+++ timm/loss/Dloss.py
@@ -11,7 +11,7 @@ from logic_seg_utils import *
 class DRuleLoss(nn.Module):
     def __init__(self, path_to_csv_tree):
         super(DRuleLoss, self).__init__()
-        self.H = get_H_matrix(path_to_csv_tree)
+        self.H,_,_ = get_tree_matrix(path_to_csv_tree)
         self.branches = torch.minimum(torch.sum(self.H, dim=1), torch.tensor(1))
         self.branch_count = torch.sum(self.branches)
 
