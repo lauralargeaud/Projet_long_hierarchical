a7c23bec45858300a93a54aa3fa7fda81779d0cf
Ruben-CRC <rub.crico@orange.fr>
2025-02-24 15:35:23 +0100

    v1>6

--- inference.py
+++ inference.py
@@ -331,8 +331,7 @@ def main():
             La_raw = get_layer_matrix(args.csv_tree, verbose=False) 
             La = torch.tensor(La_raw).to(device) # (hauteur, nb_noeuds); La[i,j] = 1 si le noeud d'index j est de profondeur i, sinon 0
             h = La.shape[0] # hauteur de l'
-            # labels_par_hauteur = [[index_to_node[j] for j in range(La.shape[1]) if int(La[hauteur,j].item()) == 1 ] for hauteur in range(h)] # liste de h sous-listes; labels_par_hauteur[i] = les labels de la hauteur
-            labels_par_hauteur = list(index_to_node.values())
+            labels_par_hauteur = [[index_to_node[j] for j in range(La.shape[1]) if int(La[hauteur,j].item()) == 1 ] for hauteur in range(h)] # liste de h sous-listes; labels_par_hauteur[i] = les labels de la hauteur
             print("lph: ", labels_par_hauteur)
             print("lph[1]: ", labels_par_hauteur[1])
             cm_par_hauteur_ids_preds = np.empty((h,0), dtype=np.float32)
@@ -520,7 +519,7 @@ def main():
             for hauteur in range(h):
                 cm = load_confusion_matrix(os.path.join(args.results_dir, "confusion_matrix_"+str(hauteur)+".out"))
                 output_filename = "confusion_matrix_"+str(hauteur)+".jpg"
-                save_confusion_matrix(cm, output_filename, labels_par_hauteur, folder="./results")
+                save_confusion_matrix(cm, output_filename, labels_par_hauteur[hauteur], folder="./results")
 
 
 def save_results(df, results_filename, results_format='csv', filename_col='filename'):
