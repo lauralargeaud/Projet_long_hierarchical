39b78eec2c006de4afdcbf0db2bc9341df9a5a3d
Ruben-CRC <rub.crico@orange.fr>
2025-02-27 11:22:19 +0100

    cm shape

--- inference.py
+++ inference.py
@@ -550,6 +550,7 @@ def main():
                 output_filename_norm = "cm_im_norm_"+header_list[hauteur]+".jpg"
                 save_confusion_matrix(cm_normalized, output_filename, labels_par_hauteur[hauteur], folder=args.results_dir)
                 save_confusion_matrix(cm_normalized, output_filename_norm, labels_par_hauteur[hauteur], folder=args.results_dir)
+            print(cm.shape)
             next_df = save_metrics(cm, folder=args.results_dir, filename=f"metrics_{header_list[hauteur]}.csv", classes=labels_par_hauteur[hauteur], hierarchy_name="hauteur_"+header_list[hauteur])
             df = pd.concat([df, next_df])
         
