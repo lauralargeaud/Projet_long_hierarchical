b5b0865a1a7c404cf66615cf7d6d3ebb064c3a14
ClementH <clement.herbin@hotmail.com>
2025-02-27 11:03:49 +0100

    fix nb batch inference.py

--- inference.py
+++ inference.py
@@ -322,6 +322,7 @@ def main():
     labels_par_hauteur = None
     use_probs = args.output_type == 'prob'
     metrics_hierarchy = None
+    nb_batches = 0
     with torch.no_grad():
         if args.logicseg:
             # top1 = 0
@@ -329,7 +330,6 @@ def main():
             H_raw, _, _ = get_tree_matrices(args.csv_tree, verbose=False)
             metrics_hierarchy = MetricsHierarchy(H_raw)
             metrics_hierarchy.setZero()
-            nb_batches = 0
             # construire la laebl_matrix
             label_matrix, _, index_to_node = get_label_matrix(args.csv_tree)
             class_to_label = get_class_to_label(label_matrix, index_to_node)
